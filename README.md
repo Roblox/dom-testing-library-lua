b8dc7c655b73e0614a46548009aae1a09d322df5
Anonymous Author <roflection-bot@roblox.com>
2022-06-19 22:25:06 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dom-testing-library-lua
+
+* Port version [v8.14.0](https://github.com/testing-library/dom-testing-library/tree/v8.14.0)
---
+++ bin/spec.lua
@@ -0,0 +1,18 @@
+local ProcessService = game:GetService("ProcessService")
+local Root = game:GetService("ReplicatedStorage")
+
+local Packages = Root.Packages
+-- Load JestGlobals source into Packages folder so it's next to Roact as expected
+local TestEZ = require(Root.Packages.Dev.JestGlobals).TestEZ
+
+-- Run all tests, collect results, and report to stdout.
+local result = TestEZ.TestBootstrap:run(
+	{ Packages.DomTestingLibrary },
+	TestEZ.Reporters.TextReporterQuiet
+)
+
+if result.failureCount == 0 and #result.errors == 0 then
+	ProcessService:ExitAsync(0)
+else
+	ProcessService:ExitAsync(1)
+end
